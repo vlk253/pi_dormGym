ab5c5f62df35c3a15788c0b7c088e60bda354ec7
vlk253 <nikolinavlk2005@gmail.com>
2026-06-23 03:15:59 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,9 @@
 // src/firebase/config.js
-// ⚠️  Zamijeni s tvojim Firebase projektom!
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics, isSupported } from "firebase/analytics"
 
-// Import the functions you need from the SDKs you need
-
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB4B_oE1YayM5GHAslSu7cT4nKAbttbARQ",
   authDomain: "gymapp-21eca.firebaseapp.com",
@@ -20,11 +12,12 @@ const firebaseConfig = {
   messagingSenderId: "770969281288",
   appId: "1:770969281288:web:3ae10d2732c5ded851c2bd",
   measurementId: "G-TLBQQ4ZCP4"
-};
+}
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig)
+
+// Analytics samo u browseru, ne u Node.js
+isSupported().then(yes => { if (yes) getAnalytics(app) })
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
